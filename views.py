6d25b53626ed66b645ed26734fa6227ee890ddfa
Romero <rrcb@cin.ufpe.br>
2024-01-10 22:27:52 -0300

    feat: add solver view

--- views.py
+++ views.py
@@ -6,3 +6,7 @@ bp = Blueprint('sudoku', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/solver')
+def solver():
+    return render_template('solver.html')
