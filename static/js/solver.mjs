89784f3aa9d57b4c85c9caa8c6f632f124d62864
Romero <rrcb@cin.ufpe.br>
2024-01-13 11:24:54 -0300

    feat: create logic to get table state on keyDown

--- static/js/solver.mjs
+++ static/js/solver.mjs
@@ -1,7 +1,41 @@
 const sudokuTable = document.getElementById('sudoku-table')
 const cells = document.querySelectorAll('.cell')
 
-sudokuTable.addEventListener('click', function(e) {
+const replaceByIndex = function(string, index, replacement) {
+    if (index >= string.length) {
+        return string
+    }
+
+    const first_part = string.slice(0, index) 
+    const second_part = string.slice(index+1)
+    const ret =  first_part + replacement + second_part
+    return ret
+}
+
+const cellPositionToIndex = function(row, col) {
+    return row*9 + col
+}
+
+const getTableState = function() {
+    let state = ''.padStart(81, '.')
+    console.log("len", state.length)
+    cells.forEach(function(cell) {
+        const cellRow = parseInt(cell.getAttribute('row'))
+        const cellCol = parseInt(cell.getAttribute('col'))
+        if (isNaN(cellRow) || isNaN(cellCol)) {
+            return 
+        }
+        const index = cellPositionToIndex(cellRow, cellCol)
+        const cellContent = cell.innerText
+        if (cellContent && !isNaN(cellContent)) {
+            let stateTemp = replaceByIndex(state, index, cellContent)
+            state = stateTemp
+        }
+    })
+    return state
+}
+
+const clickCellHandler = function(e) {
     const elem = e.target
     if (!elem.classList.contains('cell')) {
         return
@@ -14,9 +48,9 @@ sudokuTable.addEventListener('click', function(e) {
     const elemBlock = elem.getAttribute('block')
     const shadowElems = document.querySelectorAll(`[row="${elemRow}"], [col="${elemCol}"], [block="${elemBlock}"]`);
     shadowElems.forEach(elem => elem.classList.add('shadow'))
-})
+}
 
-sudokuTable.addEventListener('keydown', function(e) {
+const keyCellHandler = function(e) {
     const elem = e.target
     if (!elem.classList.contains('cell')) {
         return
@@ -29,8 +63,13 @@ sudokuTable.addEventListener('keydown', function(e) {
         return
     }
     elem.innerText = e.key;
-})
+    const tableState = getTableState()
+    console.log(tableState)
+    
+}
 
+sudokuTable.addEventListener('click', clickCellHandler)
+sudokuTable.addEventListener('keydown', keyCellHandler)
 
 const clearButton = document.getElementById('clear-button')
 clearButton.addEventListener('click', function(e) {
