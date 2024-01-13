e97b731cdbae33c682cf642c8683347c5b755096
Romero <rrcb@cin.ufpe.br>
2024-01-12 23:42:55 -0300

    feat: create table strucure
    With this it will be able to remove the hard coded html and make changes to sudoku html easily

--- views.py
+++ views.py
@@ -9,4 +9,23 @@ def index():
 
 @bp.route('/solver')
 def solver():
-    return render_template('solver.html')
+    """
+    Table structure is a list of blocks.
+    Each block is a list of dictionaries representing the block's cells.
+    Each dictionary has the 0-indexed row and col position of the cell.
+    [
+    [{}, {}..., {}],
+    [{}, {}..., {}],
+    ...
+    [{}, {}..., {}]
+    ]
+    """
+    table_structure = [
+        [
+            {'row': cell//3 + (block // 3)*3, 'col': cell%3 + (block % 3)* 3}
+            for cell in range(9)
+        ]
+        for block in range(9)
+    ]
+    print(table_structure)
+    return render_template('solver.html', table_structure=table_structure)
