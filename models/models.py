a2e9fae36769272434874053d2307f42a919007b
Romero <rrcb@cin.ufpe.br>
2024-01-22 11:28:23 -0300

    refactor: remove unnecessary packages and files

--- models/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class SudokuTable(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    difficulty = db.Column(db.String(20), nullable=False)
-    initial_state = db.Column(db.String(81), nullable=False, unique=True)
-    final_state = db.Column(db.String(81), unique=True)
